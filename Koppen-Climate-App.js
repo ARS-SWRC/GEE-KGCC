3a1caad9c3ebf80415aad2320defcfccfaa9dfe0
afullhart <58483164+afullhart@users.noreply.github.com>
2025-09-02 10:56:50 -0700

    Update Koppen-Climate-App.js

--- Koppen-Climate-App.js
+++ Koppen-Climate-App.js
@@ -35,7 +35,7 @@ var wintr_months = ee.List([1, 2, 3, 10, 11, 12]);
 /*******************************************************
   MAIN FUNCTION
 */
-var kcModule = require('users/andrewfullhart/KOPPEN:Koppen-Climate-MainFunc');
+var kcModule = require('users/gerardo/default:KOPPEN/Koppen-Climate-MainFunc');
 
 /*******************************************************
   UI
